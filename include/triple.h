b1ac1e9888df37026779e4b13f94286a1c5097fe
zxiao <zxiao@fandm.edu>
2022-03-14 19:07:35 +0000

    almost done

--- include/triple.h
+++ include/triple.h
@@ -3,38 +3,13 @@
 #include <vector>
 #include <unordered_map>
 
-struct root{
-    std::unordered_map<int, struct l0*> s_root;
-    std::unordered_map<int, struct l1*> p_root;
-    std::unordered_map<int, struct l2*> o_root;
-};
-
-struct l0{
-    std::unordered_map<int, struct l1*> s_children;
-};
-
-struct l1{
-    std::unordered_map<int, struct l2*> s_children;
-    std::unordered_map<int, struct l2*> p_children;
-};
-
-struct l2{
-    std::unordered_map<int, struct l3*> p_children;
-    std::unordered_map<int, struct l3*> o_children;
-};
-
-struct l3{
-    std::vector<int> o_children;
-};
-
 struct Triple {
-    int spo[3];
+    int s,p,o;
 } Triple;
 
 // trie node
 struct TrieNode{
-    int id;
-    std::vector<TrieNode*> s_children, p_children, o_children;
+    std::unordered_map<int, struct TrieNode*> children;
 };
 
 struct Trie{
--- triple.cpp
+++ triple.cpp
@@ -2,143 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <stack>
 #include <queue>
+
 #include "include/triple.h"
-
-/*
-
-struct TrieNode* seek(struct TrieNode *root, struct Triple triple, int offset){
-    struct TrieNode *pCrawl = root;
-    for(int i = 0; i < offset+1; i++){
-        for(const auto& child: pCrawl->s_children){
-            if(child->id == triple.spo[i]){
-                pCrawl = child;
-                break;
-            }
-        }
-    }
-    return pCrawl;
-}
-
-void updateRoot_p(struct TrieNode *root, struct TrieNode *newNode){
-    for(const auto& child: root->p_children){
-        if(child->id == newNode->id){
-            return;
-        }
-    }
-    bool inserted = false;
-    int counter = 0;
-    for(const auto& child: root->p_children){ // do insertion on an appropriate postion
-        if(child->id > newNode->id){
-            inserted = true;
-            root->p_children.insert(root->p_children.begin()+counter, newNode);
-        }
-        counter++;
-    }
-    if(!inserted){}
-        root->p_children.push_back(newNode);
-}
-
-void insert_s(struct Trie *root, struct Triple triple){
-    struct TrieNode *pCrawl = root->s_root;
-
-    std::vector<TrieNode*> spo;
-    for (int i = 0; i < 3; i++){
-        bool find = false;
-        for(const auto& child: pCrawl->s_children){ // locate the node with spo[i] in s_children
-            if(child->id == triple.spo[i]){
-                find = true;
-                pCrawl = child;
-                spo.push_back(child);
-                break;
-            }
-        }
-        if(!find){
-            struct TrieNode* newChild = new TrieNode();
-            newChild->id = triple.spo[i];          
-            spo.push_back(newChild);
-            bool inserted = false;
-            int counter = 0;
-            for(const auto& child: pCrawl->s_children){
-                if(child->id > triple.spo[i]){
-                    inserted = true;
-                    pCrawl->s_children.insert(pCrawl->s_children.begin()+counter, newChild);
-                    break;
-                }
-                counter++;
-            }
-            if(!inserted)
-                pCrawl->s_children.push_back(newChild);
-            pCrawl = newChild;
-        }
-    }
-    
-    updateRoot_p(root->p_root, spo[1]); // update root for index on p
-
-    //root->p_root->p_children.push_back(spo[1]);
-    spo[1]->p_children.push_back(spo[2]);
-    spo[2]->p_children.push_back(spo[0]);
-    //std::cout << spo[0]->id << "  " << spo[1]->id << "  " << spo[2]->id <<std::endl;
-}
-
-void insert_p(struct Trie *root, struct Triple triple){
-    struct TrieNode *pCrawl = seek(root->s_root, triple, 1); // loacte the start on s
-    updateRoot_p(root->p_root, pCrawl); // update root for index on p
-
-    // locate the node in children in s-based index
-    for(const auto& child: pCrawl->s_children){
-        if(child->id == triple.spo[2]){ // if they are equal
-            struct TrieNode* newChild = new TrieNode();
-            newChild->id = triple.spo[2];          
-            bool inserted = false;
-            int counter = 0;
-            for(const auto& child: pCrawl->p_children){ // do insertion on an appropriate postion
-                if(child->id > triple.spo[2]){
-                    inserted = true;
-                    pCrawl->p_children.insert(pCrawl->p_children.begin()+counter, newChild);
-                }
-                counter++;
-            }
-            if(!inserted){}
-                pCrawl->p_children.push_back(newChild);
-            pCrawl = newChild;
-            break;
-        }
-    }        
-
-}
-
-void print_s(struct TrieNode* root){
-    std::queue<TrieNode*> myqueue;
-    myqueue.push(root);
-
-    // Printing content of queue
-    while (!myqueue.empty()) {
-        TrieNode* top = myqueue.front();
-        for(const auto& child: top->s_children){
-            myqueue.push(child);
-        }
-        std::cout << top->id << std::endl;
-        myqueue.pop();
-
-    }   
-}
-
-void print_p(struct TrieNode* root){
-    std::queue<TrieNode*> myqueue;
-    myqueue.push(root);
-
-    // Printing content of queue
-    while (!myqueue.empty()) {
-        TrieNode* top = myqueue.front();
-        for(const auto& child: top->p_children)
-            myqueue.push(child);
-        std::cout << top->id << std::endl;
-        myqueue.pop();
-
-    }   
-}
-*/
 
 namespace mapExt{
     template<typename myMap>
@@ -157,92 +24,115 @@ namespace mapExt{
     }
 }
 
-bool check_elm(std::vector<int> v, int key){
-    return (std::find(v.begin(), v.end(), key) == v.end()) ? false : true;
+void insert(struct Trie* root, struct Triple triple){
+    int s = triple.s; int p = triple.p; int o = triple.o;
+    bool l3Flag = false; bool l4Flag = false;
+
+    // create root for s
+    if(!mapExt::check_key(root->s_root->children, s))
+        root->s_root->children[s] = new struct TrieNode();
+    struct TrieNode* l0 = root->s_root->children[s];
+
+    // create root for p
+    if(!mapExt::check_key(l0->children, p))
+        l0->children[p] = new struct TrieNode();
+    struct TrieNode* l1 = l0->children[p];
+    
+    if(!mapExt::check_key(root->p_root->children, p)){
+        root->p_root->children[p] = l1;
+        l1->children[o] = new struct TrieNode();
+    }
+    else{
+        l1->children[o] = new struct TrieNode();
+        if(!mapExt::check_key(root->p_root->children, o))
+            root->p_root->children[p]->children[o] = l1->children[o];
+        else
+            l3Flag = true;
+    }
+    struct TrieNode* l2 = l1->children[o];
+
+    if(!mapExt::check_key(root->o_root->children, o)){
+        root->o_root->children[o] = l2;
+        l2->children[s] = new struct TrieNode();
+    }
+    else{
+        l2->children[s] = new struct TrieNode();
+        if(!mapExt::check_key(root->o_root->children, s))
+            root->o_root->children[o]->children[s] = l2->children[s];
+        else
+            l4Flag = true;
+    }
+    struct TrieNode* l3 = l2->children[s];
+    
+    l3->children[p] = new struct TrieNode();
+    struct TrieNode* l4 = l3->children[p];
+    
+    if(l3Flag){
+        root->p_root->children[p]->children[o]->children[s] = l3;
+    }
+    if(l4Flag){
+        root->o_root->children[o]->children[s]->children[p] = l4;
+    }
 }
 
-void get_all_keys(std::unordered_map<int, struct l2*> map){
+void get_all_keys(std::unordered_map<int, struct TrieNode*> map){
     std::vector<int> keys;
     keys.reserve(map.size());
-
     for(auto kv : map) {
         keys.push_back(kv.first);
-        std::cout << kv.first <<std::endl;
+        std::cout << kv.first << "  ";
     } 
 }
 
+void print(struct TrieNode* root){
+    std::queue<TrieNode*> myqueue;
+    myqueue.push(root);
+    // Printing content of queue
+    while (!myqueue.empty()) {
+        TrieNode* top = myqueue.front();
 
-void insert(struct root* root, struct Triple triple){
-    struct l1* l1;
-    struct l2* l2;
-    struct l3* l3;
+        for(auto kv : top->children) {
+            myqueue.push(kv.second);
+            std::cout << kv.first << "  ";
+        }   
+        std::cout << "\n-------" << std::endl;
+        myqueue.pop();
 
-    if(!mapExt::check_key(root->s_root, triple.spo[0]))
-        root->s_root[triple.spo[0]] = new struct l0();
-    struct l0* l0 = root->s_root[triple.spo[0]];
-
-    if(!mapExt::check_key(l0->s_children, triple.spo[1])){
-        l1 = new struct l1();
-        l0->s_children[triple.spo[1]] = l1;
-        if(!mapExt::check_key(root->p_root, triple.spo[1]))
-            root->p_root[triple.spo[1]] = l1;
-    }
-    else{
-        l1 = l0->s_children[triple.spo[1]];
-    }
-    
-    if(!mapExt::check_key(l1->s_children, triple.spo[2])){
-        l2 = new struct l2();
-        l1->s_children[triple.spo[2]] = l2;
-        if(mapExt::check_key(root->p_root, triple.spo[1]))
-            root->p_root[triple.spo[1]] = l1;
-        else
-            l1->p_children[triple.spo[2]] = l2;
-        if(!mapExt::check_key(root->o_root, triple.spo[2]))
-            root->o_root[triple.spo[2]] = l2;
-    }
-    else{
-        l2 = l1->s_children[triple.spo[2]];
-    }
-
-    if(!mapExt::check_key(l2->p_children, triple.spo[0])){
-        l3 = new struct l3();
-        l2->p_children[triple.spo[0]] = l3;
-        if(mapExt::check_key(root->o_root, triple.spo[2]))
-            root->o_root[triple.spo[2]] = l2;
-        else
-            l2->o_children[triple.spo[0]] = l3;
-    }
-    else{
-        l3 = l2->p_children[triple.spo[0]];
-    }
-    l3->o_children.push_back(triple.spo[0]);
-    
+    }   
 }
+
 
 int main(){
     struct Triple t1 = {2, 3, 2};
+    struct Triple t2 = {2, 3, 4};
+    struct Triple t3 = {4, 5, 2};
+    struct Triple t4 = {5, 3, 2};
+    struct Triple t5 = {2, 5, 2};
+    struct Triple t6 = {2, 2, 2};
 
-    struct root* root = new struct root();
-    struct test{
-        std::unordered_map<int, std::vector<test*>> list;
-    };
-    struct test* a = new struct test();
-    struct test* b = new struct test();
-    a->list[0].push_back(b);
+
+    struct Trie* root = new struct Trie();
+    root->s_root = new struct TrieNode();
+    root->p_root = new struct TrieNode();
+    root->o_root = new struct TrieNode();
 
     insert(root, t1);
+    insert(root, t2);
+    insert(root, t3);
+    //insert(root, t4);
+    //insert(root, t5);
+    //insert(root, t6);
 
-    struct l0* l0 = root->s_root[2];
-    struct l1* l1 = root->s_root[2]->s_children[3];
-    struct l2* l2 = root->s_root[2]->s_children[3]->s_children[2];
-    struct l3* l3 = root->s_root[2]->s_children[3]->s_children[2]->o_children[2];
+    print(root->s_root);
+    //get_all_keys(root->p_root->children);
 
-    std::cout << mapExt::check_key(root->s_root, 2) << std::endl;
-    std::cout << mapExt::check_key(root->s_root[2]->s_children, 3) << std::endl;
-    std::cout << mapExt::check_key(root->s_root[2]->s_children[3]->s_children, 2) << std::endl;
-    std::cout << mapExt::check_key(root->s_root[2]->s_children[3]->s_children[2]->o_children, 2) << std::endl;
-
+    /*
+    get_all_keys(root->s_root->children);
+    get_all_keys(root->s_root->children[2]->children);
+    get_all_keys(root->s_root->children[2]->children[3]->children);
+    get_all_keys(root->s_root->children[2]->children[3]->children[2]->children);
+    get_all_keys(root->s_root->children[2]->children[3]->children[2]->children[2]->children);
+   */
     //std::cout << mapExt::check_key(l1, 3) << std::endl;    
     //std::cout << mapExt::check_key(l2, 2) << std::endl;    
     //std::cout << mapExt::check_key(l3, 2) << std::endl;   
