fee60c2c2aa0a11dd1bd5fc9758c3d01af9f8ef3
zxiao <zxiao@fandm.edu>
2022-03-17 00:50:25 +0000

    call for the day, need to work on Ixx, and then Nsp, Np, and Nop

--- include/table.h
+++ include/table.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include <tuple>
 
 #include "triple.h"
 #include "xxh3.h"
@@ -13,33 +12,22 @@
 #define loading_factor 0.6
 
 class Table{
-    // A hash function used to hash a tuple
-    struct hash_tuple {
     
-        template <class T1, class T2>
-    
-        size_t operator()(
-            const std::tuple<T1, T2>& x)
-            const
-        {
-            return std::get<0>(x)
-                ^ std::get<1>(x);
-        }
-    };
 public:
 
     Table();
 
-    void resize();
     void insert(struct Triple t);
-    void print();
+    void resize();
     void update_Ix(std::vector<int>& vec, int key, int value);
+
+    void print_table();
+    void print_I(std::vector<int>& vec);
 
     int size;
     int num_element;
     std::vector<int*>* table;
     std::vector<int> Is, Ip, Io;
-    std::unordered_map<std::tuple<int,int>, int, hash_tuple> Isp, Iop;
 
 };
 
--- test.cpp
+++ test.cpp
@@ -2,24 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-#include <stack>
-#include <queue>
-#include <unordered_map>
-#include <tuple>
-#include <vector>
 
 #include "include/triple.h"
-#include "include/xxh3.h"
 #include "include/table.h"
 
 Table::Table(){
     this->table = new std::vector<int*>;
-    this->table->resize(2);
-    this->size = 2;
-    this->Is.resize(10);
-    this->Ip.resize(10);
-    this->Io.resize(10);
     this->num_element = 0;
+    this->table->resize(16);
+    this->size = 2;
+    this->Is.resize(8);
+    this->Ip.resize(8);
+    this->Io.resize(8);
 }
 
 void Table::resize(){
@@ -49,11 +43,10 @@ void Table::resize(){
 }
 
 void Table::update_Ix(std::vector<int>& vec, int key, int value){
-    if(std::find(vec.begin(), vec.end(), key) != vec.end()){
-        vec[key] = value;
-    }
-    else{
+    if(key > vec.size()){
         vec.resize(vec.size()<<1);
+    }
+    if(vec[key] == 0){
         vec[key] = value;
     }
 }
@@ -63,7 +56,7 @@ void Table::insert(struct Triple t){
     std::memcpy(concatenated, (char*)&t.s, sizeof(int));
     std::memcpy(concatenated+4, (char*)&t.p, sizeof(int));
     std::memcpy(concatenated+8, (char*)&t.o, sizeof(int));    
-    int i = XXH32(concatenated, 12, 0) % this->table->size();
+    int i = XXH32(concatenated, 12, 0) % this->size;
 
     if(this->num_element/this->size > loading_factor)
         this->resize();
@@ -80,31 +73,40 @@ void Table::insert(struct Triple t){
     int* T_new = new int[6];
     T_new[0] = t.s; T_new[1] = t.p; T_new[2] = t.o;
     
-    std::tuple<int, int> t1(t.s, t.p);
 
-    this->Isp[t1] = i;
-    update_Ix(this->Is, t.s, i);
-    update_Ix(this->Ip, t.p, i);
-    update_Ix(this->Io, t.o, i);
+    update_Ix(this->Is, t.s, i+1);
+    update_Ix(this->Ip, t.p, i+1);
+    update_Ix(this->Io, t.o, i+1);
 
     this->table->at(i) = T_new;
     ++this->num_element;
 }
 
-void Table::print(){
-    for (int i = 0; i < this->table->size(); i++){
+void Table::print_table(){
+    for (int i = 0; i < this->size; i++){
         if(this->table->at(i) != NULL)
-            std::cout << this->table->at(i)[0] << " " << this->table->at(i)[1] << " " << this->table->at(i)[2] << std::endl;
+            std::cout << i << ": " << this->table->at(i)[0] << " " << this->table->at(i)[1] << " " << this->table->at(i)[2] << std::endl;
     }
 }
 
+void Table::print_I(std::vector<int>& vec){
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i] != 0)
+            std::cout << vec[i]-1 << " ";
+        else
+            std::cout << " " << " ";
+    }
+    std::cout << std::endl;
+}
+
+
 int main(){
-    struct Triple t1 = {2, 3, 2};
-    struct Triple t2 = {2, 3, 4};
-    struct Triple t3 = {4, 5, 2};
-    struct Triple t4 = {5, 3, 2};
-    struct Triple t5 = {2, 5, 2};
-    struct Triple t6 = {2, 2, 2};
+    struct Triple t1 = {1, 3, 2};
+    struct Triple t2 = {2, 1, 4};
+    struct Triple t3 = {1, 1, 2};
+    struct Triple t4 = {1, 3, 4};
+    struct Triple t5 = {2, 1, 3};
+    struct Triple t6 = {1, 1, 1};
 
     Table* table = new Table();
     
@@ -116,7 +118,10 @@ int main(){
     table->insert(t5);
     table->insert(t6);
 
-    table->print();
+    table->print_table();
+    table->print_I(table->Is);
+    table->print_I(table->Ip);
+    table->print_I(table->Io);
 
     return 0;
 }
